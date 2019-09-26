852276dd8056ae87474fb5f87d81e9514fe42f6b
BigMurry <hhdiao@siom.ac.cn>
2019-09-26 12:05:42 +0800

    basic pages export

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/eth-tool' : '',
+  exportPathMap: function() {
+    return {
+      '/': { page: '/' }
+    };
+  }
+};
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+const Index = function() {
+  return (
+    <h1>Nextjs static site</h1>
+  );
+};
+
+export default Index;
