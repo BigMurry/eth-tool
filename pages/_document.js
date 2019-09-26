eab394cf35186c4e83c32dc5628d3e00caad8c55
BigMurry <hhdiao@siom.ac.cn>
2019-09-26 17:14:36 +0800

    add favico

--- pages/_document.js
+++ pages/_document.js
@@ -20,7 +20,7 @@ class MyDocument extends Document {
             rel='stylesheet'
             href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
           />
-          <link rel='shortcut icon' type='image/jpeg' href='/static/assets/favico.png' />
+          <link rel='shortcut icon' type='image/png' href='/static/assets/favico.png' />
         </Head>
         <body>
           <Main />
