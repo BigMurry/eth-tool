34c06876bac3323eb9410fb3f7cc3bfc5534bc64
BigMurry <hhdiao@siom.ac.cn>
2020-01-09 21:08:39 +0800

    fix pages

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@ module.exports = {
   assetPrefix: process.env.NODE_ENV === 'production' ? '/eth-tool' : '',
   exportPathMap: function() {
     return {
-      '/': { page: '/' }
+      '/': { page: '/' },
+      '/decode': {page: '/decode'}
     };
   }
 };
