d7027b1cfbef56966d66ab6146d98065c1429a51
BigMurry <hhdiao@siom.ac.cn>
2020-01-13 12:08:42 +0800

    decode default eth tx

--- pages/decode.js
+++ pages/decode.js
@@ -2,6 +2,10 @@ import React, {useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 import txDecoder from 'ethereum-tx-decoder';
 import {Transaction as Tx} from 'ethereumjs-tx';
 import BN from 'bignumber.js';
@@ -39,32 +43,46 @@ function formatEventValues(values, precision = 1e0) {
   return ret;
 }
 
-function decodeTx(rawTx) {
+function decodeTx(rawTx, txType) {
   const ethTx = new Tx(rawTx);
   const tx = txDecoder.decodeTx(rawTx);
-  const fnDecoder = new txDecoder.FunctionDecoder(abi);
-  const data = fnDecoder.decodeFn(tx.data);
+  if (txType === TX_TYPES.ERC20) {
+    const fnDecoder = new txDecoder.FunctionDecoder(abi);
+    const data = fnDecoder.decodeFn(tx.data);
+    tx.decodeData = formatEventValues(data);
+  }
   tx.gasPrice = tx.gasPrice.toString();
   tx.gasLimit = tx.gasLimit.toString();
   tx.value = tx.value.toString();
-  tx.decodeData = formatEventValues(data);
   return Object.assign(
     {from: '0x' + ethTx.getSenderAddress().toString('hex')},
     tx
   );
 }
 
+const TX_TYPES = {
+  DEFAULT: 10,
+  ERC20: 20
+};
+
 let Decode = function() {
   const classes = useStyles();
   const [rawTx, setRawTx] = useState('');
   const [plainTx, setPlainTx] = useState('');
+  const [txType, setTxType] = useState(10);
+  const inputLabel = React.useRef(null);
+  const [labelWidth, setLabelWidth] = React.useState(0);
+  React.useEffect(() => {
+    setLabelWidth(inputLabel.current.offsetWidth);
+  }, []);
+
   return (
     <Root>
       <div className={classes.cont}>
         <TextField
           multiline
           id='raw'
-          label='ERC20 Raw transaction'
+          label='Raw transaction'
           rows={3}
           className={classes.tx}
           value={rawTx}
@@ -72,6 +90,23 @@ let Decode = function() {
           margin='normal'
           variant='outlined'
         />
+        <FormControl variant={'outlined'} className={classes.formControl}>
+          <InputLabel ref={inputLabel} id='tx-type'>
+            Tx type
+          </InputLabel>
+          <Select
+            labelId='tx-type-label'
+            id='tx-type-select'
+            value={txType}
+            onChange={e => setTxType(e.target.value)}
+            labelWidth={labelWidth}
+          >
+            <MenuItem value={TX_TYPES.DEFAULT}>
+              <em>Eth default</em>
+            </MenuItem>
+            <MenuItem value={TX_TYPES.ERC20}>ERC20</MenuItem>
+          </Select>
+        </FormControl>
         <Button
           size={'large'}
           variant='contained'
@@ -79,7 +114,7 @@ let Decode = function() {
           className={classes.btn}
           onClick={_ => {
             try {
-              const tx = decodeTx(rawTx);
+              const tx = decodeTx(rawTx, txType);
               setPlainTx(JSON.stringify(tx, null, 2));
             } catch (e) {}
           }}>
