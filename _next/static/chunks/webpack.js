9abd76e121845522be2b3ad3fe68f770b7fb1595
BigMurry <12495406+BigMurry@users.noreply.github.com>
2020-12-16 11:15:48 +0800

    release new version

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d0544fc485470c95cabf";
+/******/ 	var hotCurrentHash = "0d95e59f429d4e60284a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
